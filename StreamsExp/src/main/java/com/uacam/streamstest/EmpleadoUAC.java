6e8aa00e368cfc11a58efbb615dee777a389132c
SERGIO NP <serganoh@uacam.mx>
2025-10-20 08:51:25 -0600

    ObjectOutputStream example added

---
+++ StreamsExp/src/main/java/com/uacam/streamstest/EmpleadoUAC.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uacam.streamstest;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author sergi
+ */
+public class EmpleadoUAC implements Serializable {
+    //private static final long serialVersionUID = 1L;
+
+    String name;
+    String email;
+    String workStation;
+
+    public EmpleadoUAC(String name, String email, String workStation) {
+        this.name = name;
+        this.email = email;
+        this.workStation = workStation;
+    }
+
+}
---
+++ StreamsExp/src/main/java/com/uacam/streamstest/StreamsTest.java
@@ -0,0 +1,117 @@
+package com.uacam.streamstest;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * @author sergi
+ */
+public class StreamsTest {
+
+    static void escribir() {
+        try {
+            OutputStream fos = new FileOutputStream("datos.txt");
+
+            for (int i = 0x10; i < 0x19; i++) {
+                fos.write(i);
+            }
+
+            fos.close();
+        } catch (FileNotFoundException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (IOException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+
+    static void leer() {
+        try {
+            InputStream fis = new FileInputStream("datos.txt");
+
+//            int val;
+//            do {
+//                val = fis.read();
+//                if (val == -1) {
+//                    System.err.println("FIN");
+//                } else {
+//                    System.err.println(val);
+//                }
+//            } while (val != -1);
+            byte[] arr = new byte[7];
+            int cuantos = fis.read(arr);
+            System.err.println(cuantos);
+            cuantos = fis.read(arr);
+            System.err.println(cuantos);
+
+        } catch (FileNotFoundException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (IOException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+
+    }
+
+    static void escribirObjeto() {
+        try (OutputStream fos = new FileOutputStream("Objetos.txt"); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+            ArrayList<String> elemList = new ArrayList<>();
+            elemList.add("hello");
+            elemList.add("from");
+            elemList.add("elemList");
+
+            double x = 3.5;
+
+            oos.writeDouble(x);
+            oos.writeUTF("Hola Mundo");
+            oos.writeObject(elemList);
+
+        } catch (FileNotFoundException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (IOException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+
+    static void leerObjeto() {
+        try (InputStream fis = new FileInputStream("Objetos.txt"); ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+            System.out.println(ois.readDouble());
+            System.out.println(ois.readUTF());
+
+            ArrayList<String> list = (ArrayList<String>) ois.readObject();
+            System.err.println(list.size());
+
+        } catch (FileNotFoundException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (IOException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (ClassNotFoundException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+
+    static void escribirEmpleado() {
+
+        var empleados = new ArrayList<EmpleadoUAC>();
+
+        empleados.add(new EmpleadoUAC("Sergio", "serganoh@uacam.mx", "FDI"));
+        empleados.add(new EmpleadoUAC("Andres", "andres@uacam.mx", "FDM"));
+        empleados.add(new EmpleadoUAC("Pedro", "pedro@uacam.mx", "FDB"));
+
+        try (OutputStream fos = new FileOutputStream("EmpleadosUAC_DB.txt"); 
+                ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            
+            oos.writeObject(empleados);
+            
+        } catch (FileNotFoundException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (IOException ex) {
+            System.getLogger(StreamsTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } 
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        escribirEmpleado();
+
+    }
+}
